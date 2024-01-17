f857417c6d5de45eafac62749e812d8a50805bda
NEKETA <molnei1996@gmail.com>
2024-01-17 12:00:24 +0300

    add_zad

---
+++ 2.py
@@ -0,0 +1,26 @@
+"""
+ В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых. Не учитывать знаки препинания и регистр символов. За основу возьмите любую статью из википедии или из документации к языку.
+"""
+import re
+from collections import Counter
+
+text = 'Hello world. Hello Python. Hello again.'
+
+
+# Удаляем знаки препинания и приводим текст к нижнему регистру
+cleaned_text = ''.join(char.lower() if char.isalpha() or char.isspace() else ' ' for char in text)
+
+# Разбиваем текст на слова и считаем их количество
+words = cleaned_text.split()
+word_counts = {}
+
+for word in words:
+    if word not in word_counts:
+        word_counts[word] = 1
+    else:
+        word_counts[word] += 1
+
+# Получаем 10 самых часто встречающихся слов
+top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+
+print(top_words)
